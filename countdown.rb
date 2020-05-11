c067f3cf292ae537fa80a198bc59747d661025a0
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-11 22:39:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@ def countdown(number)
   while number != 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    puts "HAPPY NEW YEAR!"
   end
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -11,6 +11,6 @@ def countdown_with_sleep(number)
     puts "#{number} SECOND(S)!"
     sleep 5
     number -= 1
-    puts "HAPPY NEW YEAR!"
   end
+  "HAPPY NEW YEAR!"
 end
