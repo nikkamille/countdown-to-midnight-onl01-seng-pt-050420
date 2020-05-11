89352f4dafff94f36a8414ae6ad8bfca2eec13a8
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-11 22:37:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+def countdown(number)
+  while number != 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    "HAPPY NEW YEAR!"
+  end
+end
 
-def countdown
+def countdown_with_sleep(number)
+  
 end
